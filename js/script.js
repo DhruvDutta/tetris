990a4938d6c9b6a726556d936b46ae96bde3906f
Dhruv Dutta <54740423+DhruvDutta@users.noreply.github.com>
2020-09-16 17:54:46 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,317 @@
+let list = [];
+let shapes = ['square','line','l','t']
+let active_elem_position=[];
+let active_elements;
+function launch(){
+    document.getElementById('js-not-enabled').style.display = 'none';
+    for(let i =0;i<80;i++){
+        
+        let main = document.getElementById('main');
+        let box = document.createElement('span');
+        box.setAttribute('class','grid');
+        box.setAttribute('id',i);
+        let n = document.createTextNode(`${parseInt(i/8)} , ${i%8}`);
+        list.push([parseInt(i/8),i%8]);
+        box.appendChild(n);
+        main.appendChild(box);
+    }
+}
+launch();
+
+
+function create(){
+    document.getElementById('start').style.display = 'none';
+    let shape = shapes[Math.floor(Math.random()*3)]/*Math.floor(Math.random()*4)*/
+    let pos = Math.floor(Math.random()*4);
+    let clr=['r','g','b'][Math.floor(Math.random()*3)];
+    for(let j = 0;j<4;j++){
+        if(shape=='square'){
+            let cell = document.createElement('span');
+            let r = parseInt(j/2);
+            let c = (j%2)+pos;
+            active_elem_position[j]=[r,c];
+            cell.setAttribute('class',`cell active ${clr}`);
+            cell.style.top = `${r*4}em`;
+            cell.style.left = `${c*4}em`;
+            document.getElementById('main').appendChild(cell);
+        }else if(shape=='line'){
+            let cell = document.createElement('span');
+            cell.setAttribute('class','cell');
+            let r = 0;
+            let c = pos+j;
+            active_elem_position[j]=[r,c];
+            cell.setAttribute('class',`cell active ${clr}`);
+            cell.style.top = `${r*4}em`;
+            cell.style.left = `${c*4}em`;
+            document.getElementById('main').appendChild(cell);
+        }else if(shape=='l'){
+            let cell = document.createElement('span');
+            cell.setAttribute('class','cell');
+            let r;
+            let c;
+            if(j<3){
+                r=j;
+                c=pos;
+            }else{
+                r=j-1;
+                c=pos+1;
+            }
+            active_elem_position[j]=[r,c];
+            cell.setAttribute('class',`cell active ${clr}`);
+            cell.style.top = `${r*4}em`;
+            cell.style.left = `${c*4}em`;
+            document.getElementById('main').appendChild(cell);
+        }else if(shape=='t'){
+            let cell = document.createElement('span');
+            cell.setAttribute('class','cell');
+            let r = 1;
+            let c = pos+j-1;
+            if(j==0){
+                r=0;
+                c=pos+1;
+            }
+            active_elem_position[j]=[r,c];
+            cell.setAttribute('class',`cell active ${clr}`);
+            cell.style.top = `${r*4}em`;
+            cell.style.left = `${c*4}em`;
+            document.getElementById('main').appendChild(cell);
+        }
+    }
+    movedown();
+}
+
+
+
+function movedown(){
+    active_elements = document.querySelectorAll('.active');
+    let q =setInterval(
+        function(){
+            if(movepossible('down')){
+                for(let j = 0;j<4;j++){
+                    active_elem_position[j][0]+=1;
+                    active_elements[j].style.top = `${active_elem_position[j][0]*4}em`;
+                    //active_elements[j].innerHTML=`${active_elem_position[j]}`
+                }
+            }else{
+                for(let j=0;j<4;j++){
+                    active_elements[j].classList.remove("active");
+                }
+                active_elements=[];
+                clearInterval(q);
+                setTimeout(create,500);
+
+            }
+        }
+    ,500)
+}
+function moveright(){
+    if(movepossible('right')){
+        for(let j = 0;j<4;j++){
+            active_elem_position[j][1]+=1;
+            active_elements[j].style.left = `${active_elem_position[j][1]*4}em`;
+        }
+    }
+}
+function moveleft(){
+    if(movepossible('left')){
+        for(let j = 0;j<4;j++){
+            active_elem_position[j][1]-=1;
+            active_elements[j].style.left = `${active_elem_position[j][1]*4}em`;
+        }
+    }
+}
+function del_form_list(x,y){
+    for(let i=0;i<list.length;i++){
+        if(list[i][0]==x && list[i][1]==y){
+            console.log("deleting: "+list[i])
+            list.splice(i,1);
+            return
+        }
+    }
+}
+let cells = document.querySelectorAll('.cell');
+
+function row_check(){
+    for(let r=0;r<10;r++){
+        if(list.filter(value => value[0]==r).length == 0){
+            cells = document.querySelectorAll('.cell');
+            //cells = cells.filter(value => value.style.display!='none');
+            for(let i=0;i<cells.length;i++){
+                if(cells[i].style.top ==`${r*4}em`){
+                    cells[i].style.display = 'none';
+                    let add_r=parseInt(cells[i].style.top.split("e")[0]/4);
+                    let add_c=parseInt(cells[i].style.left.split("e")[0]/4);
+                    list.push([add_r,add_c]);
+                    console.log([add_r,add_c]+" empty")
+                }
+            }
+            row_shift(r);
+            return
+        }
+    }
+}
+
+function row_shift(r){
+    r-=1;
+    while(r>0){
+        for(let i=0;i<cells.length;i++){
+            if(cells[i].style.top.split('e')[0] ==  r*4){
+                let new_r =parseInt(cells[i].style.top.split("e")[0]/4)+1;
+                let new_c =parseInt(cells[i].style.left.split("e")[0]/4);
+                cells[i].style.top = `${new_r*4}em`;
+                list.push([new_r-1,new_c]);
+                console.log([new_r-1,new_c]+" empty1")
+                del_form_list(new_r,new_c);
+                console.log([new_r,new_c]+" filled")
+                console.log(new_r,new_c);
+            }
+        }
+        r-=1;
+    }
+}
+
+
+function movepossible(direction){
+    if(direction == 'down'){
+        for(let j=0;j<4;j++){
+            let r = active_elem_position[j][0]+1;
+            let c = active_elem_position[j][1];
+            let occurance= false;
+            for(let k = 0;k<list.length;k++){
+                if(list[k][0]==r && list[k][1]==c){
+                    occurance=true
+                    break
+                }
+            }if(occurance){
+                continue
+            }else{
+                for(let i=0;i<4;i++){
+                    r= active_elem_position[i][0]+1;
+                    c=active_elem_position[i][1];
+                    del_form_list(r-1,c);
+                }
+                row_check()
+                return false
+            }
+        }
+        return true
+    }else if(direction=='right'){
+        for(let j=0;j<4;j++){
+            let r = active_elem_position[j][0];
+            let c = active_elem_position[j][1]+1;
+            let occurance= false;
+            for(let k = 0;k<list.length;k++){
+                if(list[k][0]==r && list[k][1]==c){
+                    occurance=true
+                    break
+                }
+            }if(occurance){
+                continue
+            }else{
+                return false
+            }
+        }
+        return true
+    }else if(direction=='left'){
+        for(let j=0;j<4;j++){
+            let r = active_elem_position[j][0];
+            let c = active_elem_position[j][1]-1;
+            let occurance= false;
+            for(let k = 0;k<list.length;k++){
+                if(list[k][0]==r && list[k][1]==c){
+                    occurance=true
+                    break
+                }
+            }if(occurance){
+                continue
+            }else{
+                return false
+            }
+        }
+        return true
+    }
+}
+
+
+
+$(function(){ // this will be called when the DOM is ready
+    $(document).keyup(function() {
+      let e = window.event.keyCode;
+        if(e==37){
+            moveleft();
+        }else if(e==39){
+            moveright();
+        }
+    });
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+---------------------------------------------
+use this to access all active elements
+console.log(document.querySelectorAll('.active'))
+
+---------------------------------------------
+Use this to eliminate coordinate after moving stops
+if(JSON.stringify(list).includes(JSON.stringify([x,y]))){
+    console.log('yes');
+    console.log(list[x*8+y],x,y)
+}else{
+}
+*/
+
+
+
+/*function create(shape){
+    let sh = document.createElement('span');
+    sh.setAttribute('class',shape);
+    sh.setAttribute('id','active');
+    for(let j = 0;j<4;j++){
+        let box1 = document.createElement('span');
+        box1.setAttribute('class','cell');
+        sh.appendChild(box1);
+    }
+    let top = 0;
+    let left = 0;        
+    document.getElementById("main").appendChild(sh);
+    sh.style.top = `${top}em`;
+    sh.style.left = `${left}em`;
+    movedown()
+}
+
+function movedown(){
+    let sh = document.getElementById("active");
+    let top = 0;
+    let left = 0;
+    let q = setInterval(function(){
+        if(top<8){
+            top+=1;
+            sh.style.top = `${top*4}em`;
+            sh.style.left = `${left*4}em`;
+        }else{
+            clearInterval(q);
+            console.log(top,left,sh.querySelectorAll(".cell")[0])
+        }
+    },500)
+}
+create('square')
+*/
